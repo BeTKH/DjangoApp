b1b58910648d9d7348590636c2c80a908713aaa1
Bekalu Tadesse <45674779+BeTKH@users.noreply.github.com>
2023-05-03 12:08:24 -0500

    python lists

---
+++ PyReca/DataTypes/CollectionTypes/lists/lst.py
@@ -0,0 +1,63 @@
+
+
+"""
+Lists are
+
+- ordered
+- mutable
+- allow duplicates and dt data types
+
+"""
+
+hyb_list = [1,2,3,3, "str", True, False, False, 3.14]
+
+for i in hyb_list:
+    print('list item', i, "has data type:", type(i))
+
+print()
+#list operations
+
+#check if item exists
+
+list_Fruit = ["apple", "banana", "cherry"]
+if "apple" in list_Fruit:
+  print("Yes, 'apple' is in the fruits list")
+print()
+
+#mutate / modify list at a given index
+list_Fruit[0] = "Mango"
+print(list_Fruit)
+
+list_Fruit[0:2] = ["Kiwi", "Avocado"]
+print(list_Fruit)
+
+
+#insert into list
+list_Fruit = ["apple", "banana", "cherry"]
+list_Fruit.insert(0, "Mangooo")
+print(list_Fruit, )
+
+
+#append into list
+list_Fruit = ["apple", "banana", "cherry"]
+list_Fruit.append("Mangooo")
+print(list_Fruit, "\n")
+
+#extend list
+
+list1 = ["A", "B", "C"]
+list2 = ["D", "E"]
+
+list1.extend(list2)
+print(list1)
+
+
+list1 = ["A", "B"]
+list2 = ["D", "E"]
+list_3 = list1 + list2
+print(list_3)
+
+
+# remove
+list_3.remove("A")
+print(list_3)
